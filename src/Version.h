f634e0d2437977010b0a17c9e38e9e29faf35dd1
Matt Carlen <matt@printm3d.com>
2018-06-19 18:15:18 -0400

    Set version information -- internal/RC status

--- src/Version.h
+++ src/Version.h
@@ -9,13 +9,13 @@
 #define SRC_VERSION_H_
 
 #ifndef VERSION
-# define VERSION "1.21"
+# define VERSION "1M.23-RC7"
 #endif
 
 #ifndef DATE
-# define DATE "2018-03-21"
+# define DATE "2018-06-14"
 #endif
 
-#define AUTHORS "reprappro, dc42, chrishamm, t3p3, dnewman"
+#define AUTHORS "reprappro, dc42, chrishamm, t3p3, dnewman, 3dmatt, printm3d"
 
 #endif /* SRC_VERSION_H_ */
