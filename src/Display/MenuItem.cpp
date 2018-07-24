dcd75fe5df06c6181c73208720ecb8b76ec30eaa
Matt Carlen <matt@printm3d.com>
2018-07-24 17:34:33 -0400

    File listing on 12864 LCD display
    TODO: does not enter subdirectories
    
    # Conflicts:
    #	src/Display/Menu.cpp
    #	src/Display/MenuItem.h
    #	src/Display/ST7920/lcd7920.cpp

--- src/Display/MenuItem.cpp
+++ src/Display/MenuItem.cpp
@@ -38,8 +38,12 @@ void ButtonMenuItem::Draw(Lcd7920& lcd, PixelNumber rightMargin, bool highlight)
 {
 	lcd.SetCursor(row, column);
 	lcd.SetRightMargin(rightMargin);
+
 	lcd.TextInvert(highlight);
-	lcd.print(text);
+	lcd.print(text); // TODO: create Print(char[], bool) to combine these two lines
+
+	lcd.TextInvert(false);
+	lcd.ClearToMargin();
 }
 
 ValueMenuItem::ValueMenuItem(PixelNumber r, PixelNumber c, FontNumber fn, PixelNumber w, unsigned int v, unsigned int d)
@@ -180,17 +184,195 @@ bool ValueMenuItem::Adjust(int clicks)
 
 FilesMenuItem *FilesMenuItem::freelist = nullptr;
 
-FilesMenuItem::FilesMenuItem(PixelNumber r, PixelNumber c, FontNumber fn, const char *cmd, const char *dir, unsigned int nf)
-	: MenuItem(r, c, fn), initialDirectory(dir), numFiles(nf)
+FilesMenuItem::FilesMenuItem(PixelNumber r, PixelNumber c, FontNumber fn, const char *cmd, const char *dir, unsigned int nf, unsigned int uFontHeight)
+	: MenuItem(r, c, fn), command(cmd), initialDirectory(dir), m_uDisplayLines(nf), m_uFontHeight(uFontHeight),
+        m_uFirstFileVisible(0), m_uCurrentSelectedFile(0), m_oMS(reprap.GetPlatform().GetMassStorage())
 {
+	m_acCommand[0] = '\0';
+
+	EnterDirectory(initialDirectory);
+}
+
+void FilesMenuItem::EnterDirectory(const char *acDir)
+{
+	m_uTotalFilesInCurrentDirectory = 0;
+
+	// TODO: these lens may need to be +1 for the null character
+	SafeStrncpy(m_acCurrentDirectory, acDir, MaxFilenameLength);
+
+	FileInfo oFileInfo;
+	if (m_oMS->FindFirst(m_acCurrentDirectory, oFileInfo))
+	{
+		do
+		{
+			++m_uTotalFilesInCurrentDirectory;
+		}
+		while (m_oMS->FindNext(oFileInfo));
+	}
 }
 
 void FilesMenuItem::Draw(Lcd7920& lcd, PixelNumber rightMargin, bool highlight)
 {
 	lcd.SetCursor(row, column);
 	lcd.SetRightMargin(rightMargin);
-	lcd.print("File list here...");
-	//TODO
+
+	// We are writing text to line numbers 0, 1, 2 ... m_uDisplayLines - 1
+	// These are file entries m_nCurrentSelectedFile, m_nCurrentSelectedFile + 1, m_nCurrentSelectedFile + 2 ... m_nCurrentSelectedFile + m_uDisplayLines - 1 within the directory
+
+	// TODO: this must not be run when the SD card is ejected!
+
+	// Seek to the first file that is in view
+	int nDirListingLocation = -1;
+	FileInfo oFileInfo;
+	if (m_oMS->FindFirst(m_acCurrentDirectory, oFileInfo))
+	{
+		do
+		{
+			++nDirListingLocation;
+		}
+		// Must short-circuit in this order!
+		while ((nDirListingLocation < static_cast<int>(m_uFirstFileVisible)) && m_oMS->FindNext(oFileInfo));
+	}
+
+	// TODO: if not current directory same as initial directory, include item ".." on the list
+	//   This also becomes a special case for Select()
+
+	// TODO: allow sorting by filename?  seems to be output in order of file creation
+
+	bool bFileSystemError = false;
+	for (uint8_t i = 0; (i < m_uDisplayLines) && !bFileSystemError; ++i, bFileSystemError = !(m_oMS->FindNext(oFileInfo)))
+	{
+		lcd.SetCursor(row + (m_uFontHeight * i), column);
+
+		// char acFileName[18];
+
+		if (m_uTotalFilesInCurrentDirectory > i + m_uFirstFileVisible)
+		{
+			// lcd.TextInvert(highlight);
+			if (highlight && (m_uCurrentSelectedFile == (i + m_uFirstFileVisible)))
+			{
+				lcd.print("> ");
+			}
+			else
+			{
+				lcd.print("  ");
+			}
+
+			if (oFileInfo.isDirectory)
+				lcd.print("./");
+
+			lcd.print(oFileInfo.fileName);
+			// lcd.print(m_nFirstFileVisible + i);
+			// lcd.TextInvert(false);
+		}
+
+		lcd.ClearToMargin();
+	}
+
+	// TODO: cache these filenames to avoid the SD overhead each time...
+}
+
+void FilesMenuItem::Enter(bool bForwardDirection)
+{
+	if (bForwardDirection)
+	{
+		m_uCurrentSelectedFile = 0;
+		m_uFirstFileVisible = 0;
+	}
+	else
+	{
+		// TODO: graceful handling of empty directory
+		m_uCurrentSelectedFile = m_uTotalFilesInCurrentDirectory - 1;
+		m_uFirstFileVisible = ((m_uTotalFilesInCurrentDirectory > m_uDisplayLines) ? (m_uTotalFilesInCurrentDirectory - m_uDisplayLines) : 0);
+	}
+}
+
+int FilesMenuItem::Advance(int nCounts)
+{
+	while (nCounts > 0)
+	{
+		// Advancing one more would take us past the end of the list
+		// Instead, return the remaining count so that the other
+		// selectable menu items can be scrolled.
+		if (m_uTotalFilesInCurrentDirectory == m_uCurrentSelectedFile + 1)
+			break;
+
+		++m_uCurrentSelectedFile;
+		--nCounts;
+
+		// Move the visible portion of the list down, if required
+		if (m_uCurrentSelectedFile == m_uFirstFileVisible + m_uDisplayLines)
+			++m_uFirstFileVisible;
+	}
+
+	while (nCounts < 0)
+	{
+		if (0 == m_uCurrentSelectedFile)
+			break;
+
+		--m_uCurrentSelectedFile;
+		++nCounts;
+
+		// Move the visible portion of the list up, if required
+		if (m_uCurrentSelectedFile < m_uFirstFileVisible)
+			--m_uFirstFileVisible;
+	}
+
+	return nCounts;
+}
+
+const char* FilesMenuItem::Select()
+{
+	// Several cases:
+	// 1. File - run command with filename as argument
+	// TODO 2. Directory - call EnterDirectory(), adding to saved state information
+	// TODO 3. ".." entry - call EnterDirectory(), using saved state information
+
+	// Get information on the item selected
+
+	// TODO: this must not be allowed when the SD card is ejected!
+
+	// Seek to the first file that is in view
+	int nDirectoryLocation = -1;
+	FileInfo oFileInfo;
+	if (m_oMS->FindFirst(m_acCurrentDirectory, oFileInfo))
+	{
+		do
+		{
+			++nDirectoryLocation;
+		}
+		while ((nDirectoryLocation != static_cast<int>(m_uCurrentSelectedFile)) && m_oMS->FindNext(oFileInfo)); // relying on short-circuit -- don't change order!
+	}
+
+	if (oFileInfo.isDirectory)
+	{
+		return nullptr;
+	}
+	else
+	{
+		m_acCommand[0] = '\0';
+
+		SafeStrncpy(m_acCommand, command, strlen(command) + 1);
+
+		int nReplacementIndex = StringContains(m_acCommand, "#0");
+		if (-1 != nReplacementIndex)
+		{
+			nReplacementIndex -= strlen("#0");
+
+			SafeStrncpy(m_acCommand + nReplacementIndex, "\"", 2);
+			++nReplacementIndex;
+
+			SafeStrncpy(m_acCommand + nReplacementIndex, oFileInfo.fileName, strlen(oFileInfo.fileName) + 1);
+			nReplacementIndex += strlen(oFileInfo.fileName);
+
+			SafeStrncpy(m_acCommand + nReplacementIndex, "\"", 2);
+			// ++nReplacementIndex // unused
+
+			// debugPrintf("Attempting: %s\n", m_acCommand);
+		}
+
+		return m_acCommand; // would otherwise break encapsulation, but the return is const
+	}
 }
 
 // End
